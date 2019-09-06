ab2d7883a3cbb8439081ec39eba43ca08a9c0c39
Ian Shepherd <ianshepherd77@gmail.com>
2019-09-06 19:08:37 +0100

    added team and ratings

--- js/app.js
+++ js/app.js
@@ -5,11 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const handleNewItemFormSubmit = function (event) {
   event.preventDefault();
-
-  const playerListItem = createPlayerListItem(event.target);
   const playerList = document.querySelector('#player-list');
+  const playerListItem = createPlayerListItem(event.target);
   playerList.appendChild(playerListItem);
-
   event.target.reset();
 }
 
@@ -17,9 +15,21 @@ const createPlayerListItem = function (form) {
   const playerListItem = document.createElement('li');
   playerListItem.classList.add('player-list-item');
 
-  const firstName = document.createElement('h2');
-  firstName.textContent = form.first_name.value;
-  playerListItem.appendChild(firstName);
+  const playerFirstName = document.createElement('h2');
+  playerFirstName.textContent = form.first_name.value;
+  playerListItem.appendChild(playerFirstName);
+
+  const playerLastName = document.createElement('h3');
+  playerLastName.textContent = form.last_name.value;
+  playerListItem.appendChild(playerLastName);
+
+  const playerTeam = document.createElement('h4');
+  playerTeam.textContent = form.team_name.value;
+  playerListItem.appendChild(playerTeam);
+
+  const playerRating = document.createElement('p');
+  playerRating.textContent = form.rating.value;
+  playerListItem.appendChild(playerRating);
 
   // const author = document.createElement('h3');
   // author.textContent = form.author.value;
